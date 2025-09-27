43816528f88625a9a63b21a99b69dbe9f993fe94
Ramiro Santiago Toulemonde <ramirotule@gmail.com>
2025-09-27 10:24:33 -0300

    updating email

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -72,7 +72,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="mailto:ramirotoulemonde@gmail.com"
+                  href="mailto:ramiro.toulemonde@gmail.com"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
